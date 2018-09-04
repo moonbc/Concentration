233d973b4d654c52ffb1651d34a57d6c830d7c16
moonbc_mac <lightbc@gmail.com>
2018-09-04 23:49:25 +0900

    Add touchCard Button Action

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        print("agh! ahost!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
