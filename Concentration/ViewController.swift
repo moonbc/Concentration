36d2baa499d290bb4131c223a58a0d7bf8daa6ef
moonbc_mac <lightbc@gmail.com>
2018-09-05 00:58:54 +0900

    Add flipCountLabel

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    var filpCount = 0 {
+        //fileCount가 변할 때 마다 라벨을 업데이트시킨다.
+        didSet {
+            flipCountLabel.text = "Flips: \(filpCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
     @IBAction func touchCard(_ sender: UIButton) {
+        filpCount += 1
         flipCard(withEmoji: "👻", on: sender)
         print("agh! ahost!")
     }
     
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        filpCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    
     func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji)")
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControlState.normal)
             button.backgroundColor =  #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
