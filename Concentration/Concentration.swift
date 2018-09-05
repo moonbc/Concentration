efad18b4850afd011cf9b3828be64567951ad970
moonbc_mac <lightbc@gmail.com>
2018-09-06 02:12:23 +0900

    Implemented chooseCard method

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,11 +12,27 @@ class Concentration
 {
     var cards = [Card]()
     
+    var indexOfOnAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        }else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOnAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOnAndOnlyFaceUpCard = nil
+            }else {
+                //either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOnAndOnlyFaceUpCard = index
+            }
         }
     }
     
