f695445815a38469e2bc64b27f7a864cffafd0b8
moonbc_mac <lightbc@gmail.com>
2018-09-18 23:59:18 +0900

    add PlayingCard class

---
+++ Concentration/PlayingCard.swift
@@ -0,0 +1,28 @@
+//
+//  PlayingCard.swift
+//  Concentration
+//
+//  Created by moonbc on 2018. 9. 18..
+//  Copyright © 2018년 moonbc.com. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard
+{
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String {
+        
+        case spades = "♠︎"
+        case hearts = "❤️"
+        case diamonds = "♦︎"
+        case clubs = "♣︎"
+    }
+    enum Rank {
+        
+    }
+
+}
+
